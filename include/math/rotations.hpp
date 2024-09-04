e3f17370f8e72b28c3dcaf043713dd235cc667fd
vvaza22 <vvaza22@freeuni.edu.ge>
2024-09-05 02:07:18 +0400

    Add rotations around x and z axes

--- include/math/rotations.hpp
+++ include/math/rotations.hpp
@@ -13,4 +13,24 @@ namespace math {
       0.0f               , 0.0f, 0.0f              , 1.0f
     });
   }
+
+  // Creates a rotation matrix around the x-axis
+  matrixslayer::Mat rotationX(float angleRad) {
+    return matrixslayer::Matrix4f({
+      1.0f, 0.0f              , 0.0f               , 0.0f,
+      0.0f, std::cos(angleRad), -std::sin(angleRad), 0.0f,
+      0.0f, std::sin(angleRad), std::cos(angleRad) , 0.0f,
+      0.0f, 0.0f              , 0.0f               , 1.0f
+    });
+  }
+
+  // Creates a rotation matrix around the z-axis
+  matrixslayer::Mat rotationZ(float angleRad) {
+    return matrixslayer::Matrix4f({
+      std::cos(angleRad) , -std::sin(angleRad), 0.0f              , 0.0f,
+      std::sin(angleRad) , std::cos(angleRad) , 0.0f              , 0.0f,
+      0.0f               , 0.0f               , 1.0f              , 0.0f,
+      0.0f               , 0.0f               , 0.0f              , 1.0f
+    });
+  }
 }
